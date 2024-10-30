bd13a52a0cebc67edf3b747beff0f154fd20c9ce
zhan <testmailbox500@gmail.com>
2024-10-30 14:39:40 +0300

    Update .gitignore to exclude .env and environment folder

--- Intermediate(30-45 days)/6_partSix/2_PandaLibrary/3.0_ Grouping&AggregatingData.py
+++ Intermediate(30-45 days)/6_partSix/2_PandaLibrary/3.0_ Grouping&AggregatingData.py
@@ -1,13 +1,13 @@
-# import pandas as pd
+import pandas as pd
 
-# # Sample dataset
-# sales_data = {
-#     "Product": ["A", "B", "A", "B", "C"],
-#     "Region": ["North", "North", "South", "South", "North"],
-#     "Sales": [100, 150, 80, 120, 60]
-# }
-# df = pd.DataFrame(sales_data)
+# Sample dataset
+sales_data = {
+    "Product": ["A", "B", "A", "B", "C"],
+    "Region": ["North", "North", "South", "South", "North"],
+    "Sales": [100, 150, 80, 120, 60]
+}
+df = pd.DataFrame(sales_data)
 
-# # Group by Product and calculate the total sales for each
-# grouped = df.groupby("Product")["Sales"].sum()
-# print("Total Sales by Product:\n", grouped)
+# Group by Product and calculate the total sales for each
+grouped = df.groupby("Product")["Sales"].sum()
+print("Total Sales by Product:\n", grouped)
